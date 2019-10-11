2dc0ee8c93d02cafb847d3173b86d9a4ba06d6dd
kjo1 <kjo0@outlook.com>
2019-10-11 15:19:00 -0400

    1

--- app/src/main/java/nc/prog1415/MainActivity.java
+++ app/src/main/java/nc/prog1415/MainActivity.java
@@ -1,6 +1,7 @@
 package nc.prog1415;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TintableImageSourceView;
 
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -13,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView iv = findViewById(R.id.imageView);
+
+        final int[] id = [R.drawable.marsha,R.drawable.peter,R.drawable.dino];
+    }
+
+    class MyThread implements Runnable
+    {
+        @Override
+        public void run() {
+
+        }
     }
 }
