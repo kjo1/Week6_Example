2ef6574173af147fa84b3f63df625bf263adf705
kjo1 <kjo0@outlook.com>
2019-10-11 15:35:51 -0400

    2

--- app/src/main/java/nc/prog1415/MainActivity.java
+++ app/src/main/java/nc/prog1415/MainActivity.java
@@ -4,25 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.TintableImageSourceView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.ContextMenu;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo info)
+    {
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView iv = findViewById(R.id.imageView);
 
-        final int[] id = [R.drawable.marsha,R.drawable.peter,R.drawable.dino];
+        final ImageView iv = findViewById(R.id.imageView);
+
+        final int ids[] = {R.drawable.marsha, R.drawable.peter, R.drawable.dino};
+        int index = 0;
+        Runnable intro = new Runnable() {
+            @Override
+            public void run() {
+                final int subscript = index;
+                iv.setImageResource(ids[subscript]);
+                if(++index < ids.length)
+                    iv.postDelayed(this, 2000);
+
+            }
+        };
+        Handler h = new Handler();
+        h.post(intro);
     }
 
-    class MyThread implements Runnable
-    {
+    class MyThread implements Runnable {
         @Override
         public void run() {
+            while (true) {
 
+            }
         }
     }
 }
